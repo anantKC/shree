68f131159c2a58e014a0ee26dc18b49fc3fab631
Anant KC <anantkc@MacBook-Air.local>
2022-05-30 20:39:02 +0545

    first loookk

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,7 +6,6 @@ import {
   faLinkedin,
   faGithub,
   faYoutube,
-  faSkype,
 } from '@fortawesome/free-brands-svg-icons'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
@@ -36,7 +35,7 @@ const Sidebar = () => {
       <ul>
         <li>
           <a
-            href="https://www.linkedin.com/in/slobodan-gaji%C4%87-006bb8b8/"
+            href="#"
             target="_blank"
             rel="noreferrer"
           >
@@ -45,7 +44,7 @@ const Sidebar = () => {
         </li>
         <li>
           <a
-            href="https://github.com/bobangajicsm"
+            href="#"
             target="_blank"
             rel="noreferrer"
           >
@@ -54,16 +53,11 @@ const Sidebar = () => {
         </li>
         <li>
           <a
-            href="https://www.youtube.com/channel/UCBu5ulO4d-d47lAVybpRTkw"
+            href="#"
             rel="noreferrer"
             target="_blank"
           >
             <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
-          </a>
-        </li>
-        <li>
-          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
-            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
           </a>
         </li>
       </ul>
